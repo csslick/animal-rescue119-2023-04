cb4ecfde0c83e151542b544475affd9f18048e53
csslick <digitone@naver.com>
2023-04-11 15:58:21 +0900

    동물목록

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,26 @@ import Home from './pages/Home';
 import Detail from './pages/Detail';
 
 function App() {
+  const [data, setData] = useState([]);
+
   const API_KEY = 'sXxYjidiN3t6GurP%2FlL532W8cmmt4qCl%2F%2BFF72uNIWACqGGmumk6enycmK39NmiGxpmGhhxqFXvWYu4zH8f3zg%3D%3D';
-  const API_URL = `http://apis.data.go.kr/6260000/BusanPetAnimalInfoService?serviceKey=${API_KEY}`;
+  const API_URL = `http://apis.data.go.kr/6260000/BusanPetAnimalInfoService/getPetAnimalInfo?serviceKey=${API_KEY}&resultType=json`;
   
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => {
+        const anim_data = data.getPetAnimalInfo.body.items.item;
+        console.log(anim_data);
+        setData(anim_data);
+      })
+  }, []);
 
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home data={data} />} />
         <Route path="/detail" element={<Detail />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props)
+  const { data } = props;
+
   return (
     <>
       <Navbar />
       <div className='container'>
         <h1>Home</h1>
+        <ul>
+          <li>
+            <img src={data[0].ty3Picture} />
+            <h4>{data[0].sj}</h4>
+            <p>{data[0].ty3Kind}</p>
+            <p>{data[0].ty3Process}</p>
+          </li>
+        </ul>
+        {/* ty3Picture 사진
+        ty3Kind 품종
+        sj 기본정보
+        ty3Process 진행상태 */}
       </div>
     </>
     
